89809767c6fbb9a2282d4e13bb7910b3f5f99aef
Mikhail Shatikhin <shatikhin@skbkontur.ru>
2018-05-19 03:59:29 +0500

    olo

--- script.js
+++ script.js
@@ -1,224 +1,334 @@
-var limit = 5;
-var max = 20;
-var count = 0;
-var fail = 0;
-var height = 24;
-var lapTime = 3;
-
-var tasks = [
-    {
-        content: "Some code string 1",
-        side: "left"
-    },
-    {
-        content: "Some code string 2",
-        side: "right"
-    },
-    {
-        content: "Some code string 3",
-        side: "right"
-    },
-    {
-        content: "Some code string 4",
-        side: "left"
-    },
-    {
-        content: "Some code string 5",
-        side: "right"
-    },
-    {
-        content: "Some code string 6",
-        side: "left"
-    },
-    {
-        content: "Some code string 7",
-        side: "left"
-    },
-    {
-        content: "Some code string 8",
-        side: "right"
-    },
-    {
-        content: "Some code string 9",
-        side: "right"
-    },
-    {
-        content: "Some code string 10",
-        side: "right"
-    },
-    {
-        content: "Some code string 11",
-        side: "left"
-    },
-    {
-        content: "Some code string 12",
-        side: "right"
-    },
-    {
-        content: "Some code string 13",
-        side: "right"
-    },
-    {
-        content: "Some code string 14",
-        side: "right"
-    },
-    {
-        content: "Some code string 15",
-        side: "left"
-    },
-    {
-        content: "Some code string 16",
-        side: "left"
-    },
-    {
-        content: "Some code string 17",
-        side: "right"
-    },
-    {
-        content: "Some code string 18",
-        side: "right"
-    },
-    {
-        content: "Some code string 19",
-        side: "right"
-    },
-    {
-        content: "Some code string 20",
-        side: "right"
-    },
+const initTasks = [
+  {
+    content: 'Some code string 1',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 2',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 3',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 4',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 5',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 6',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 7',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 8',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 9',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 10',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 11',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 12',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 13',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 14',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 15',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 16',
+    side: 'left'
+  },
+  {
+    content: 'Some code string 17',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 18',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 19',
+    side: 'right'
+  },
+  {
+    content: 'Some code string 20',
+    side: 'right'
+  }
 ];
 
-var startBtn = document.querySelector(".start");
-var deck = document.querySelector(".deck");
+const App = {
+  gameId: null,
+  username: '',
+  useremail: '',
+  info: {
+    userSteps: 0,
+    errorCount: 0
+  },
+  constants: {
+    limitErrors: 5,
+    maxSteps: 20,
+    height: 24,
+    lapTime: 3
+  },
+  tasks: initTasks.slice(),
+  controls: {
+    startBtn: document.querySelector('.start'),
+    deck: document.querySelector('.deck')
+  },
+  events: {
+    init: function() {
+      App.info.errorCount = 0;
+      App.info.userSteps = 0;
+      App.tasks = initTasks.slice();
+      App.controls.deck.innerHTML = '';
+      App.controls.deck.classList.add('hidden');
+      App.controls.startBtn.classList.remove('hidden');
+    },
 
-var speed = (document.documentElement.clientHeight - height) / lapTime;
+    loose: function(data) {
+      Storage.saveScore(data);
+      HtmlHelper.renderScore();
+      alert('You lose!');
+      App.events.init();
+    },
 
-var random = function(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    win: function(data) {
+      Storage.saveScore(data);
+      HtmlHelper.renderScore();
+      alert('You win!');
+      App.events.init();
+    }
+  }
+};
+
+class Storage {
+  static saveScore(data) {
+    const scores = Storage.getScores();
+    scores[App.gameId] = data;
+    localStorage.setItem('scores', JSON.stringify(scores));
+  }
+
+  static getScore() {
+    const scores = Storage.getScores();
+    return scores[App.gameId];
+  }
+
+  static getScores() {
+    try {
+      return JSON.parse(localStorage.getItem('scores')) || {};
+    } catch (e) {
+      return {};
+    }
+  }
 }
 
-var lose = function() {
-    alert("You lose!");
-    deck.innerHTML = "";
-    deck.classList.add("hidden");
-    startBtn.classList.remove("hidden");
+class HtmlHelper {
+  static hideScores() {
+    const scores = document.querySelector('.scores-info');
+    if (scores) {
+      scores.remove();
+    }
+  }
+
+  static renderScore() {
+    const div = document.createElement('div');
+    div.className = 'scores-info';
+    const { errorCount, userSteps } = Storage.getScore();
+    div.innerHTML = `
+     <div>Ошибок: ${errorCount}</div>
+     <div>Шагов: ${userSteps}</div>
+    `;
+    document.body.appendChild(div);
+  }
+}
+
+const speed =
+  (document.documentElement.clientHeight - App.constants.height) /
+  App.constants.lapTime;
+
+const nextEvent = function() {
+  if (
+    App.info.errorCount < App.constants.limitErrors &&
+    App.info.userSteps < App.constants.maxSteps
+  ) {
+    run();
+  } else if (App.info.errorCount >= App.constants.limitErrors) {
+    App.events.loose({
+      username: App.username,
+      useremail: App.useremail,
+      errorCount: App.info.errorCount,
+      userSteps: App.info.userSteps
+    });
+  } else if (App.info.userSteps >= App.constants.maxSteps) {
+    App.events.win({
+      username: App.username,
+      useremail: App.useremail,
+      errorCount: App.info.errorCount,
+      userSteps: App.info.userSteps
+    });
+  }
 };
 
-var win = function() {
-    alert("You win!");
-    deck.innerHTML = "";
-    deck.classList.add("hidden");
-    startBtn.classList.remove("hidden");
+const animateBrick = function(brick) {
+  let endPosition =
+    App.controls.deck.clientHeight -
+    App.constants.height -
+    App.constants.height * App.info.errorCount;
+
+  let duration = endPosition / speed;
+
+  setTimeout(() => {
+    brick.classList.add('animate');
+    brick.style.transitionDuration = `${duration}s`;
+    brick.style.transform = `translateX(-50%) translateY(${endPosition}px)`;
+  });
 };
 
-var step = function() {
-    let index = random(0, tasks.length);
-    let { content, side } = tasks[index];
-    tasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
+const userGoodChoise = (brick, side) => {
+  let position = brick.getBoundingClientRect();
+  brick.style.transitionDuration = '';
+  brick.style.transform = `translateX(-50%) translateY(${position.top}px)`;
 
-    let brick = document.createElement("div");
-    brick.classList.add("brick");
-    brick.textContent = content + " / " + side;
-    brick.dataset.side = side;
-    deck.appendChild(brick);
+  let shiftX =
+    side === 'left'
+      ? -(position.left + brick.clientWidth)
+      : App.controls.deck.clientWidth - position.left;
 
-    let endPosition = (deck.clientHeight - height) - (height * fail);
-    let duration = endPosition / speed;
+  setTimeout(function() {
+    brick.style.transitionDuration = '0.5s';
+    brick.style.transform = `translateX(${shiftX}px) translateY(${
+      position.top
+    }px)`;
+  });
 
-    setTimeout(function() {
-        brick.classList.add("animate");
-        brick.style.transitionDuration = `${duration}s`;
-        brick.style.transform = `translateX(-50%) translateY(${endPosition}px)`;
-    }, 0);
+  const onSuccessTransitionEnd = function() {
+    App.info.userSteps++;
+    brick.remove();
+    nextEvent();
+    brick.removeEventListener('transitionend', onSuccessTransitionEnd);
+  };
 
-    const onDropTransitionEnd = function() {
-        brick.removeEventListener("transitionend", onDropTransitionEnd);
-        document.removeEventListener("keydown", onArrowKeyDown);
-        count++;
-        fail++;
-        brick.classList.remove("animate");
-        brick.style.transitionDuration = "";
-        if (fail < limit && count < max) {
-            step();
-        } else if (fail >= limit) {
-            lose();
-        } else if (count >= max) {
-            win();
-        }
-    };
-
-    const onArrowKeyDown = function(evt) {
-        if (evt.keyCode !== 37 && evt.keyCode !== 39) {
-            return false;
-        }
-
-        brick.removeEventListener("transitionend", onDropTransitionEnd);
-
-        document.removeEventListener("keydown", onArrowKeyDown);
-
-        let userSide = evt.keyCode === 37 ? "left" : "right";
-        let position = brick.getBoundingClientRect();
-
-        if (userSide === side) {
-            brick.style.transitionDuration = "";
-            brick.style.transform = `translateX(-50%) translateY(${position.top}px)`;
-
-            let shiftX = side === "left" ? (-(position.left + brick.clientWidth)) : (deck.clientWidth - position.left);
-
-            setTimeout(function() {
-                brick.style.transitionDuration = "0.5s";
-                brick.style.transform = `translateX(${shiftX}px) translateY(${position.top}px)`;
-            }, 0);
-
-            const onSuccessTransitionEnd = function() {
-                count++;
-                brick.remove();
-                if (fail < limit && count < max) {
-                    step();
-                } else if (fail >= limit) {
-                    lose();
-                } else if (count >= max) {
-                    win();
-                }
-                brick.removeEventListener("transitionend", onSuccessTransitionEnd);
-            };
-
-            brick.addEventListener("transitionend", onSuccessTransitionEnd);
-        } else {
-            brick.style.transitionDuration = "";
-            brick.style.transform = `translateX(-50%) translateY(${position.top}px)`;
-
-            setTimeout(function() {
-                brick.style.transitionDuration = "0.5s";
-                brick.style.transform = `translateX(-50%) translateY(${endPosition}px)`;
-            }, 0);
-
-            const onFailTransitionEnd = function() {
-                count++;
-                fail++;
-                brick.classList.remove("animate");
-                brick.style.transitionDuration = "";
-                if (fail < limit && count < max) {
-                    step();
-                } else if (fail >= limit) {
-                    lose();
-                } else if (count >= max) {
-                    win();
-                }
-                brick.removeEventListener("transitionend", onFailTransitionEnd);
-            };
-
-            brick.addEventListener("transitionend", onFailTransitionEnd);
-        }
-    };
-
-    brick.addEventListener("transitionend", onDropTransitionEnd);
-
-    document.addEventListener("keydown", onArrowKeyDown);
+  brick.addEventListener('transitionend', onSuccessTransitionEnd);
 };
 
-var start = function() {
-    startBtn.classList.add("hidden");
-    deck.classList.remove("hidden");
-    step();
+const userFailChoise = brick => {
+  let position = brick.getBoundingClientRect();
+
+  let endPosition =
+    App.controls.deck.clientHeight -
+    App.constants.height -
+    App.constants.height * App.info.errorCount;
+
+  brick.style.transitionDuration = '';
+  brick.style.transform = `translateX(-50%) translateY(${position.top}px)`;
+
+  setTimeout(function() {
+    brick.style.transitionDuration = '0.5s';
+    brick.style.transform = `translateX(-50%) translateY(${endPosition}px)`;
+  }, 0);
+
+  const onFailTransitionEnd = function() {
+    App.info.userSteps++;
+    App.info.errorCount++;
+    brick.classList.remove('animate');
+    brick.style.transitionDuration = '';
+    nextEvent();
+    brick.removeEventListener('transitionend', onFailTransitionEnd);
+  };
+
+  brick.addEventListener('transitionend', onFailTransitionEnd);
+};
+
+const createBrick = function(content, side) {
+  const brick = document.createElement('div');
+  brick.classList.add('brick');
+  brick.textContent = content + ' / ' + side;
+  brick.dataset.side = side;
+  App.controls.deck.appendChild(brick);
+
+  animateBrick(brick);
+
+  return brick;
+};
+
+const run = function() {
+  const indexRandomTask = Math.floor(Math.random() * App.tasks.length);
+  const task = App.tasks[indexRandomTask];
+  if (!task) {
+    throw new Error(
+      'Немогу взять таск ' + indexRandomTask + ';' + App.tasks.length
+    );
+  }
+  const { content, side } = task;
+  App.tasks.slice().splice(indexRandomTask, 1);
+
+  const brick = createBrick(content, side);
+
+  const onArrowKeyDown = function(evt) {
+    // игнорируем любые нажатия кроме стрелки влево вправо
+    if (evt.keyCode !== 37 && evt.keyCode !== 39) {
+      return false;
+    }
+    // удаляем события?
+    brick.removeEventListener('transitionend', onDropTransitionEnd);
+    document.removeEventListener('keydown', onArrowKeyDown);
+
+    // проверяем правильный ли выбор сдел пользователь
+    const userSide = evt.keyCode === 37 ? 'left' : 'right';
+    if (userSide === side) {
+      userGoodChoise(brick, side);
+    } else {
+      userFailChoise(brick);
+    }
+  };
+
+  document.addEventListener('keydown', onArrowKeyDown);
+
+  const onDropTransitionEnd = function() {
+    brick.classList.remove('animate');
+    brick.style.transitionDuration = '';
+
+    document.removeEventListener('keydown', onArrowKeyDown);
+    App.info.userSteps++;
+    App.info.errorCount++;
+    nextEvent();
+    brick.removeEventListener('transitionend', onDropTransitionEnd);
+  };
+
+  brick.addEventListener('transitionend', onDropTransitionEnd);
+};
+
+const start = function() {
+  HtmlHelper.hideScores();
+  App.username = prompt('Как вас зовут?', '');
+  App.useremail = prompt('Ваша почта?', '');
+  App.gameId = Date.now();
+  App.controls.startBtn.classList.add('hidden');
+  App.controls.deck.classList.remove('hidden');
+  run();
 };
